a9fa049b97e2012aa16dbef92cc3938a9f4690a1
Artiom Ponomariov <artushock@gmail.com>
2021-08-25 09:43:31 +0300

    Configured Kotlin in a project

--- app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
+++ app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.artushock.moviesearcher;
+package com.artushock.moviesearcher
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artushock.moviesearcher.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }

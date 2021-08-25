306eca4f3d63923c1204f1b05ef131cfd45ac353
Artiom Ponomariov <artushock@gmail.com>
2021-08-25 15:30:09 +0300

    Added listener to button

--- app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
+++ app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
@@ -2,11 +2,16 @@ package com.artushock.moviesearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.artushock.moviesearcher.R
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.press_btn).setOnClickListener{
+            findViewById<TextView>(R.id.hello_tv).text = getString(R.string.button_pressed)
+        }
     }
 }

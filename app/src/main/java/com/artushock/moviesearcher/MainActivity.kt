1374426dbb6ebc7c94a694df143fd36f7397a438
Artiom Ponomariov <artushock@gmail.com>
2021-08-26 11:16:04 +0300

    Added Recycler View to project

--- app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
+++ app/src/main/java/com/artushock/moviesearcher/MainActivity.kt
@@ -1,17 +1,58 @@
 package com.artushock.moviesearcher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.press_btn).setOnClickListener{
-            findViewById<TextView>(R.id.hello_tv).text = getString(R.string.button_pressed)
-        }
+        initData()
+        initView()
+    }
+
+    private fun initView() {
+        initButtons()
+        initRecyclerView()
+    }
+
+    private fun initButtons() {
+        
+    }
+
+    private fun initRecyclerView() {
+        val movieRecyclerView: RecyclerView = findViewById(R.id.movie_list_rv)
+        movieRecyclerView.setHasFixedSize(true)
+
+        val layoutManager: LinearLayoutManager = LinearLayoutManager(this)
+        movieRecyclerView.layoutManager = layoutManager
+
+        val movieAdapter: MoviesAdapter = MoviesAdapter(MovieList)
+
+        movieRecyclerView.adapter = movieAdapter
+    }
+
+    private fun initData() {
+        MovieList.movies.add(Movie("Побег из Шоушенка", MovieGenre.DRAMA, 10))
+        MovieList.movies.add(Movie("Крёстный отец", MovieGenre.ACTION, 10))
+        MovieList.movies.add(Movie("Начало", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Москва слехам не верит", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Побег из Шоушенка", MovieGenre.DRAMA, 10))
+        MovieList.movies.add(Movie("Крёстный отец", MovieGenre.ACTION, 10))
+        MovieList.movies.add(Movie("Начало", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Москва слехам не верит", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Побег из Шоушенка", MovieGenre.DRAMA, 10))
+        MovieList.movies.add(Movie("Крёстный отец", MovieGenre.ACTION, 10))
+        MovieList.movies.add(Movie("Начало", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Москва слехам не верит", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Побег из Шоушенка", MovieGenre.DRAMA, 10))
+        MovieList.movies.add(Movie("Крёстный отец", MovieGenre.ACTION, 10))
+        MovieList.movies.add(Movie("Начало", MovieGenre.ACTION, 9))
+        MovieList.movies.add(Movie("Москва слехам не верит", MovieGenre.ACTION, 9))
     }
 }
---
+++ app/src/main/java/com/artushock/moviesearcher/MovieGenre.kt
@@ -0,0 +1,14 @@
+package com.artushock.moviesearcher
+
+enum class MovieGenre {
+    ACTION,
+    WESTERN,
+    GANGSTER,
+    DETECTIVE,
+    DRAMA,
+    HISTORICAL,
+    COMEDY,
+    MELODRAMA,
+    MUSIC,
+    NOIR
+}

1de93db1dd12eae92b1ea9a1fbc26b52f359c518
Artiom Ponomariov <artushock@gmail.com>
2021-09-27 16:26:50 +0300

    Added showing contact list to the project

--- app/src/main/java/com/artushock/moviesearcher/view/fragments/ContactsFragment.kt
+++ app/src/main/java/com/artushock/moviesearcher/view/fragments/ContactsFragment.kt
@@ -1,29 +1,42 @@
 package com.artushock.moviesearcher.view.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ContentResolver
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.artushock.moviesearcher.R
 import com.artushock.moviesearcher.databinding.FragmentContactsBinding
+
+const val REQUEST_CODE = 42
 
 class ContactsFragment : Fragment() {
 
     private var _binding: FragmentContactsBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentContactsBinding.inflate(inflater, container, false)
-        return binding?.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //checkPermission()
+        checkPermission()
     }
 
     companion object {
@@ -37,8 +50,94 @@ class ContactsFragment : Fragment() {
     }
 
     private fun checkPermission() {
-        TODO("Not yet implemented")
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                    getContacts()
+                }
+
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Для совершения звонков из приложения необходимо предоставить доступ к Вашим контактам")
+                        .setPositiveButton("Ок") { _, _ ->
+                            requestPermission()
+                        }
+                        .setNegativeButton("Отмена") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .create()
+                        .show()
+                }
+                else -> {
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun getContacts() {
+        context?.let {
+            val contentResolver: ContentResolver = it.contentResolver
+
+            val cursorWithContacts: Cursor? = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+
+            cursorWithContacts?.let { cursor ->
+                for (i in 0..cursor.count) {
+                    if (cursor.moveToPosition(i)) {
+                        val name =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+
+                        addView(it, name)
+                    }
+                }
+            }
+            cursorWithContacts?.close()
+        }
+    }
+
+    private fun addView(context: Context, textToShow: String?) {
+        binding.containerForContacts.addView(AppCompatTextView(context).apply {
+            text = textToShow
+            textSize = resources.getDimension(R.dimen.contcts_text_size)
+        })
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts()
+                } else {
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Разрешение отсутсвует")
+                            .setMessage("Звонки из приложения невозможны из-за отстувтия разрешения")
+                            .setNegativeButton("Закрыть") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+                    }
+                }
+                return
+            }
+        }
     }
 }
-
-

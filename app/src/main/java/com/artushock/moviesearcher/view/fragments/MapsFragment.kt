a01975d3b2b7764c4c3e6c3de357026640d0a787
Artiom Ponomariov <artushock@gmail.com>
2021-10-01 11:52:32 +0300

    Rewrote map searching with coroutines

--- app/src/main/java/com/artushock/moviesearcher/view/fragments/MapsFragment.kt
+++ app/src/main/java/com/artushock/moviesearcher/view/fragments/MapsFragment.kt
@@ -2,26 +2,23 @@ package com.artushock.moviesearcher.view.fragments
 
 import android.location.Geocoder
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.artushock.moviesearcher.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MapsFragment : Fragment() {
-
-    val TAG = "MapsFragment"
-    private lateinit var listener: LocationListener
 
     private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(51.282742, 30.208725)
@@ -31,13 +28,11 @@ class MapsFragment : Fragment() {
 
     private val searchCallback = OnMapReadyCallback { googleMap ->
         val address = getAddressStr()
-        Log.d(TAG, "searchCallback - address: $address")
-        Thread { getLatLngByAddress(address) }.start()
-        listener = object : LocationListener {
-            override fun onLocationGotten(location: LatLng) {
-                googleMap.addMarker(MarkerOptions().position(location).title("Marker in Sydney"))
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
-            }
+
+        lifecycleScope.launch {
+            val latLng = async { getLatLngByAddress(address) }
+            googleMap.addMarker(MarkerOptions().position(latLng.await()).title("Marker in Sydney"))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng.await(), 12f))
         }
     }
 
@@ -63,29 +58,12 @@ class MapsFragment : Fragment() {
             }
     }
 
-    private fun getLatLngByAddress(address: String) {
-        val handler = Handler(Looper.getMainLooper())
-
-        try {
-            val geocoder = Geocoder(context)
-            val location = geocoder.getFromLocationName(address, 1)
-
-            Log.d(TAG, "getLatLngByAddress - location.size: ${location.size}")
-            handler.post {
-                if (location.isNotEmpty()) {
-                    Log.d(TAG, "getLatLngByAddress in handler - location.size: ${location.size}")
-                    with(location[0]) {
-                        listener.onLocationGotten(LatLng(latitude, longitude))
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            e.stackTrace
+    private fun getLatLngByAddress(address: String): LatLng {
+        val geocoder = Geocoder(context)
+        val location = geocoder.getFromLocationName(address, 1)
+        with(location[0]) {
+            return LatLng(latitude, longitude)
         }
-    }
-
-    interface LocationListener {
-        fun onLocationGotten(location: LatLng)
     }
 }
 
